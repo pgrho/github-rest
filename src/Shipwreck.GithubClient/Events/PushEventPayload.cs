1aed42b5021f8cd11fb3e60c093f1c5a68d8efef
pgrho <shipwreck@live.jp>
2017-09-28 10:37:59 +0900

    Added PushEventPayload webhook API properties

--- src/Shipwreck.GithubClient/Events/PushEventPayload.cs
+++ src/Shipwreck.GithubClient/Events/PushEventPayload.cs
@@ -2,7 +2,6 @@ using System;
 using System.ComponentModel;
 using System.Runtime.Serialization;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 
 namespace Shipwreck.GithubClient.Events
 {
@@ -21,6 +20,30 @@ namespace Shipwreck.GithubClient.Events
         [DataMember, JsonProperty("before")]
         public string Before { get; set; }
 
+        [DefaultValue(null)]
+        [DataMember, JsonProperty("after")]
+        public string After { get; set; }
+
+        [DefaultValue(null)]
+        [DataMember, JsonProperty("created")]
+        public bool? Created { get; set; }
+
+        [DefaultValue(null)]
+        [DataMember, JsonProperty("deleted")]
+        public bool? Deleted { get; set; }
+
+        [DefaultValue(null)]
+        [DataMember, JsonProperty("forced")]
+        public bool? Forced { get; set; }
+
+        [DefaultValue(null)]
+        [DataMember, JsonProperty("base_ref")]
+        public string BaseRef { get; set; }
+
+        [DefaultValue(null)]
+        [DataMember, JsonProperty("compare")]
+        public string Compare { get; set; }
+
         [DefaultValue(0)]
         [DataMember, JsonProperty("size")]
         public int Size { get; set; }
@@ -32,5 +55,13 @@ namespace Shipwreck.GithubClient.Events
         [DefaultValue(null)]
         [DataMember, JsonProperty("commits")]
         public Commit[] Commits { get; set; }
+
+        [DefaultValue(null)]
+        [DataMember, JsonProperty("head_commit")]
+        public Commit HeadCommit { get; set; }
+
+        [DefaultValue(null)]
+        [DataMember, JsonProperty("pusher")]
+        public Committer Pusher { get; set; }
     }
 }
